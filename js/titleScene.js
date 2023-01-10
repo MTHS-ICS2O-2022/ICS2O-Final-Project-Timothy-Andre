4b951e5e93bd9d64f3fdb7ffcbd01c7c9b997946
Timothy Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2023-01-10 14:40:14 -0500

    title screen fix 1

--- js/titleScene.js
+++ js/titleScene.js
@@ -2,42 +2,44 @@
 
 // Copyright (c) 2023 TMAD All rights reserved
 //
-//Created by: Timothy Manwell
+//Created by: Timothy Manwell and Andre Deveau
 //Created on: Jan 2023
-//This is the Title Scene
+//This is the Splash Scene
 
 /**
- * This class is the title scene
+ * This class is the splash scene
  */
-class TitleScene extends Phaser.Scene {
-    /**
-     * This method is the constructor
-     */
-    constructor() {
-      super({ key: "titleScene" })
-      this.titleSceneBackgroundImage = null
-    }
-  
-    init(data) {
-      this.cameras.main.setBackgroundColor("#ffffff")
-    }
-  
-    preload() {
-      console.log("Title Scene")
-      this.load.image("titleSceneBackground", "./assets/title.png")
-    }
-  
-    create(data) {
-      this.titleSceneBackgroundImage = this.add
-        .sprite(0, 0, "titleSceneBackground")
-        .setScale(2.75)
-    }
-  
-    update(time, delta) {
-      if (time > 6000) {
-        this.scene.switch("menuScene")
-      }
-    }
+class SplashScene extends Phaser.Scene {
+  /**
+   * This method is the constructor
+   */
+  constructor() {
+    super({ key: "titleScene" });
+
+    this.titleSceneBackgroundImage = null
   }
-  export default TitleScene
-  
+
+  init(data) {
+    this.cameras.main.setBackgroundColor("#ffffff")
+  }
+
+  preload() {
+    console.log("Title Scene")
+    this.load.image("titleSceneBackground", "./assets/title.png")
+  }
+
+  create(data) {
+    this.titleSceneBackgroundImage = this.add.sprite(
+      0,
+      0,
+      "titleSceneBackground"
+    );
+    this.titleSceneBackground.x = 1920 / 2
+    this.titleSceneBackground.y = 1080 / 2
+  }
+
+  update(time, delta) {
+  }
+}
+
+export default TitleScene
