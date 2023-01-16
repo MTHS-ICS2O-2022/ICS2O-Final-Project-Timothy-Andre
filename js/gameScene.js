c217b2895563d26dc7dd21e28d456602a568e3b3
Timothy Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2023-01-16 09:42:12 -0500

    game test

--- js/gameScene.js
+++ js/gameScene.js
@@ -3,22 +3,13 @@
 // Copyright (c) 2022 Timothy Manwell All rights reserved
 //
 //Created by: Timothy Manwell
-//Created on: Nov 2022
+//Created on: Jan 2023
 //This is the Game Scene
 
 /**
  * This class is the game scene
  */
 class GameScene extends Phaser.Scene {
-    createAlien () {
-      const alienXLocation = Math.floor(Math.random() * 1920) + 1
-      let alienXVelocity = Math.floor(Math.random() * 50) + 1
-      alienXVelocity *= Math.round(Math.random()) ? 1 : -1
-      const anAlien = this.physics.add.sprite(alienXLocation, -100, 'alien')
-      anAlien.body.velocity.y = 200
-      anAlien.body.velocity.x = alienXVelocity
-      this.alienGroup.add(anAlien)
-    }
     /**
      * This method is the constructor
      */
@@ -26,12 +17,7 @@ class GameScene extends Phaser.Scene {
       super({ key: "gameScene" })
 
       this.background = null
-      this.ship = null
-      this.fireMissile = false
-      this.score = 0
-      this.scoreText = null
-      this.scoreTextStyle = { font: "65px Arial", fill: "#ffffff", align: "center" }
-      this.gameOverTextStyle = { font: "65px Arial", fill: "#ff0000", align: "center" }
+      this.darcy = null
       }
   
     init(data) {
@@ -42,88 +28,48 @@ class GameScene extends Phaser.Scene {
       console.log("Game Scene")
 
       //images
-      this.load.image("starBackground", "./assets/starBackground.png")
-      this.load.image("ship", "./assets/spaceShip.png")
-      this.load.image("missile", "./assets/missile.png")
-      this.load.image("alien", "./assets/alien.png")
-
-      //audio
-      this.load.audio("laser", "./assets/laser1.wav")
-      this.load.audio("explosion", "./assets/barrelExploding.wav")
-      this.load.audio("bomb", "./assets/bomb.wav")
+      this.load.image("tableCloth", "./assets/tableCloth.jpg")
+      this.load.image("darcy", "./assets/spaceShip.png")
     }
   
     create(data) {
-      this.background = this.add.image(0, 0, "starBackground").setScale(2.0)
+      this.background = this.add.image(0, 0, "tableCloth").setScale(3.0)
       this.background.setOrigin(0, 0)
 
-      this.scoreText = this.add.text(10, 10, "Score: " + this.score.toString(), this.scoreTextStyle)
-
-      this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
-
-      this.missileGroup = this.physics.add.group()
-
-      this.alienGroup = this.add.group()
-      this.createAlien()
-
-      this.physics.add.collider(this.missileGroup, this.alienGroup, function (missileCollide, alienCollide) {
-        alienCollide.destroy ()
-        missileCollide.destroy ()
-        this.sound.play('explosion')
-        this.score = this.score + 1
-        this.scoreText.setText("Score: " + this.score.toString())
-        this.createAlien ()
-        this.createAlien ()
-      }.bind(this))
-
-      this.physics.add.collider(this.ship, this.alienGroup, function (shipCollide, alienCollide) {
-        this.sound.play('bomb')
-        this.physics.pause()
-        alienCollide.destroy()
-        shipCollide.destroy()
-        this.gameOverText = this.add.text(1920 / 2, 1080 / 2, "Game Over!\nClick to play again.", this.gameOverTextStyle).setOrigin(0.5)
-        this.gameOverText.setInteractive({ useHandCursor: true })
-        this.gameOverText.on("pointerdown", () => this.scene.start("gameScene"))
-      }.bind(this))
+      this.darcy = this.physics.add.sprite(1920 / 2, 1080 - 100, "darcy")
     }
   
     update(time, delta) {
       const keyLeftObj = this.input.keyboard.addKey("LEFT")
       const keyRightObj = this.input.keyboard.addKey("RIGHT")
-      const keySpaceObj = this.input.keyboard.addKey("SPACE")
+      const keyUpObj = this.input.keyboard.addKey("UP")
+      const keyDownObj = this.input.keyboard.addKey("DOWN")
+  
       
       if (keyLeftObj.isDown === true) {
-        this.ship.x -= 15
-        if (this.ship.x < 0) {
-          this.ship.x = 0
+        this.darcy.x -= 15
+        if (this.darcy.x < 0) {
+          this.darcy.x = 0
         }
       }
       if (keyRightObj.isDown === true) {
-        this.ship.x += 15
-        if (this.ship.x > 1920) {
-          this.ship.x = 1920
+        this.darcy.x += 15
+        if (this.darcy.x > 1920) {
+          this.darcy.x = 1920
         }
       }
-      if (keySpaceObj.isDown === true) {
-        if (this.fireMissile === false) {
-          this.fireMissile = true
-          const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, "missile")
-          this.missileGroup.add(aNewMissile)
-          this.sound.play("laser")
+      if (keyDownObj.isDown === true) {
+        this.darcy.y -= 15
+        if (this.darcy.y < 1080) {
+          this.darcy.y = 1080
         }
       }
-
-      if (keySpaceObj.isUp === true) {
-        this.fireMissile = false
-      }
-
-      this.missileGroup.children.each(function (item) {
-        item.y = item.y - 15
-        if (item.y < 0) {
-          item.destroy()
+      if (keyUpObj.isDown === true) {
+        this.darcy.y += 15
+        if (this.darcy.y > 0) {
+          this.darcy.y = 0
         }
-      })
+      }
     }
 }
-  export default GameScene
-  
+export default GameScene
