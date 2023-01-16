f59808548afb7260723a808f5496054b4a065c53
Timothy Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2023-01-16 14:29:29 -0500

    bigger background

--- js/gameScene.js
+++ js/gameScene.js
@@ -46,7 +46,7 @@ class GameScene extends Phaser.Scene {
     }
   
     create(data) {
-      this.background = this.add.image(0, 0, "earth").setScale(2.0)
+      this.background = this.add.image(0, 0, "earth").setScale(5.0)
       this.background.setOrigin(0, 0)
 
       this.darcy = this.physics.add.sprite(1920 / 2, 1080 - 100, "darcy")
