82f18f439dd832273ad19a62360bc97e02283bdd
Timothy Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2023-01-17 14:49:10 -0500

    idk

--- js/gameScene.js
+++ js/gameScene.js
@@ -81,7 +81,6 @@ class GameScene extends Phaser.Scene {
       this.physics.add.collider(this.darcy, this.hazardGroup, function (darcyCollide, hazardCollide) {
         this.physics.pause()
         hazardCollide.destroy()
-        darcyCollide.destroy()
         this.gameOverText = this.add.text(1920 / 2, 1080 / 2, "Game Over!\nClick to play again.", this.gameOverTextStyle).setOrigin(0.5)
         this.gameOverText.setInteractive({ useHandCursor: true })
         this.gameOverText.on("pointerdown", () => this.scene.start("gameScene"))
