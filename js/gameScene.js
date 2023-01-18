bf619deb337f83bd05955711df3495af9ec26c31
Timothy Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2023-01-18 13:40:37 -0500

    hazard test

--- js/gameScene.js
+++ js/gameScene.js
@@ -74,13 +74,13 @@ class GameScene extends Phaser.Scene {
         this.score = this.score + 1
         this.scoreText.setText("Score: " + this.score.toString())
         this.createFood ()
-        this.createHazard ()
         this.sound.play("nom")
       }.bind(this))
 
       this.physics.add.collider(this.darcy, this.hazardGroup, function (darcyCollide, hazardCollide) {
         this.physics.pause()
         hazardCollide.destroy()
+        darcyCollide.destroy ()
         this.gameOverText = this.add.text(1920 / 2, 1080 / 2, "Game Over!\nClick to play again.", this.gameOverTextStyle).setOrigin(0.5)
         this.gameOverText.setInteractive({ useHandCursor: true })
         this.gameOverText.on("pointerdown", () => this.scene.start("gameScene"))
