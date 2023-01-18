f47732a7189ff33b5e2b03e47626e9f467c8538d
Timothy Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2023-01-18 14:05:45 -0500

    score reset

--- js/gameScene.js
+++ js/gameScene.js
@@ -82,6 +82,8 @@ class GameScene extends Phaser.Scene {
         this.physics.pause()
         hazardCollide.destroy()
         darcyCollide.destroy ()
+        this.score = this.score - score
+        this.scoreText.setText("Score: " + this.score.toString())
         this.gameOverText = this.add.text(1920 / 2, 1080 / 2, "Game Over!\nClick to play again.", this.gameOverTextStyle).setOrigin(0.5)
         this.gameOverText.setInteractive({ useHandCursor: true })
         this.gameOverText.on("pointerdown", () => this.scene.start("gameScene"))
